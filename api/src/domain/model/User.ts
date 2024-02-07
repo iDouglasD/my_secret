aa0f28a9bef69099fce907d64cc43a3a51dacf29
Douglas Duarte <douglasduarte.eng@gmail.com>
2024-02-07 06:32:29 -0300

    feat: create migrations and initial config user entity.

---
+++ api/src/domain/model/User.ts
@@ -0,0 +1,10 @@
+export interface UserModel {
+  userId: number;
+  name: string | null;
+  email: string;
+  password: string;
+  pictureUrl: string | null;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
+}
